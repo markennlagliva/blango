ce5a1de8bd31b13a455cee7521c1093c492b1a74
Markenn Lagliva <mkenndave@gmail.com>
2023-11-18 04:35:56 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -113,6 +113,9 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+
+        'django.contrib.sites', # This is for Allauth
+
         'django.contrib.staticfiles',
 
         'blango_auth',
@@ -120,7 +123,19 @@ class Dev(Configuration):
         'crispy_forms',
         'crispy_bootstrap5',
         'debug_toolbar',
+
+        # --Django sites
+        'allauth',
+        'allauth.account',
+        'allauth.socialaccount',
+        'allauth.socialaccount.providers.google',
     ]
+    
+    SITE_ID = 1
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
 
     CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
     CRISPY_TEMPLATE_PACK = "bootstrap5"
